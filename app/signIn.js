f2f3b01cd50f8e5c7a23bfed46f8e1e2f8ddfbfc
Abinash Sonar <74831561+abinash0013@users.noreply.github.com>
2024-09-19 22:50:19 +0530

    signup design complete

--- app/signIn.js
+++ app/signIn.js
@@ -1,9 +1,26 @@
 import { Octicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar'
-import { View, Text, Image, TextInput, TouchableOpacity, Pressable } from 'react-native'
+import { useRef, useState } from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity, Pressable, Alert } from 'react-native'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Loading from '../components/Loading';
 
 export default function SignIn() {
+  const router = useRouter();
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async () => {
+    if(!emailRef.current || passwordRef.current){
+      Alert.alert("Sign In", "Please fill all the fields")
+      return;
+    }
+    // login process 
+  }
+ 
   return (
     <View className="flex-1"> 
       <StatusBar style="dark" />
@@ -17,6 +34,7 @@ export default function SignIn() {
             <View style={{height:hp(7)}} className="flex-row px-4 bg-neutral-100 items-center rounded-2xl">
               <Octicons name="mail" color={"gray"} size={hp(2.7)} />
               <TextInput
+                onChange={()=>emailRef.current=value}
                 style={{fontSize: hp(2), marginLeft: 5}}
                 className="flex-1 font-semibold text-neutral-700"
                 placeholder="Enter Email"
@@ -26,18 +44,28 @@ export default function SignIn() {
             <View style={{height:hp(7)}} className="flex-row px-4 bg-neutral-100 items-center rounded-2xl">
               <Octicons name="lock" color={"gray"} size={hp(2.7)} />
               <TextInput
+                onChange={()=>passwordRef.current=value}
                 style={{fontSize: hp(2), marginLeft: 5}}
                 className="flex-1 font-semibold text-neutral-700"
                 placeholder="Enter Password"
+                secureTextEntry
                 placeholderTextColor={'gray'}
               />
             </View>
           </View> 
           <View className="gap-4">
             <Text style={{fontSize: hp(1.8)}} className="font-bold text-right text-neutral-500 pt-3">Forgot Password.!</Text>
-            <TouchableOpacity style={{height: hp(6.5)}} className="bg-indigo-500 rounded-xl justify-center items-center">
-              <Text style={{fontSize: hp(2.7)}} className="text-white font-bold tracking-wider">Sign In</Text>
-            </TouchableOpacity>
+            <View>
+              {loading ? (
+                <View className="flex-row justify-center">
+                  <Loading size={50} />
+                </View>
+              ) : (
+                <TouchableOpacity onPress={handleLogin} style={{height: hp(6.5)}} className="bg-indigo-500 rounded-xl justify-center items-center">
+                  <Text style={{fontSize: hp(2.7)}} className="text-white font-bold tracking-wider">Sign In</Text>
+                </TouchableOpacity>
+              )}
+            </View>
             <View className="flex-row justify-center">
               <Text style={{fontSize: hp(1.8)}} className="font-semibold text-neutral-500">Don't have an account </Text>
               <Pressable>
